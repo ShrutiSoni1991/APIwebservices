141c04d3d822332d1e150da61603b5cfdc41f8f0
ShrutiSoni1991 <shruti9575@gmail.com>
2018-04-28 17:25:09 -0500

    New SpringBoot application

---
+++ src/main/java/io/springboot/learning/startup/topic/TopicRepository.java
@@ -0,0 +1,12 @@
+package io.springboot.learning.startup.topic;
+
+public interface TopicRepository {
+	
+	
+	//getAlltopics()
+	//getTopic(String id)
+	//updateTopics(Topic t)
+	//deleteTopic(String id)
+	
+	
+}
